60f66f33d0b83414c7af4baeef1d02cb9ef1b18d
Sean Cross <sean@xobs.io>
2019-08-13 11:42:30 +0800

    dependencies: don't install qt and other nextpnr deps
    These aren't needed here.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,10 @@
 # Install dependencies script
 
- BOOST="libboost-dev libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev libboost-python-dev libboost-iostreams-dev libboost-system-dev libboost-chrono-dev libboost-date-time-dev libboost-atomic-dev libboost-regex-dev"
-
 if [ $ARCH == "linux_x86_64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
-                          gcc-5 g++-5 libeigen3-dev qtbase5-dev libpython3.5-dev
+                          xdot pkg-config python3 \
+                          gcc-5 g++-5
   sudo apt-get autoremove -y
   sudo update-alternatives \
     --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 \
@@ -18,9 +16,8 @@ fi
 if [ $ARCH == "linux_i686" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
-                          gcc-5-multilib g++-5-multilib libeigen3-dev qtbase5-dev libpython3.5-dev
-  sudo ln -s /usr/include/asm-generic /usr/include/asm
+                          xdot pkg-config python3 \
+                          gcc-5-multilib g++-5-multilib
   sudo apt-get autoremove -y
   sudo update-alternatives \
     --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 \
@@ -32,9 +29,9 @@ fi
 if [ $ARCH == "linux_armv7l" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
+                          xdot pkg-config python3 \
                           gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf \
-                          binfmt-support qemu-user-static libeigen3-dev qtbase5-dev libpython3.5-dev
+                          binfmt-support qemu-user-static
   sudo apt-get autoremove -y
   arm-linux-gnueabihf-gcc --version
   arm-linux-gnueabihf-g++ --version
@@ -43,9 +40,9 @@ fi
 if [ $ARCH == "linux_aarch64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
+                          xdot pkg-config python3 \
                           gcc-aarch64-linux-gnu g++-aarch64-linux-gnu \
-                          binfmt-support qemu-user-static libeigen3-dev qtbase5-dev libpython3.5-dev
+                          binfmt-support qemu-user-static
   sudo apt-get autoremove -y
   aarch64-linux-gnu-gcc --version
   aarch64-linux-gnu-g++ --version
@@ -54,9 +51,8 @@ fi
 if [ $ARCH == "windows_x86" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
-                          gcc-5-mingw-w64 gc++-5-mingw-w64 wine libeigen3-dev qtbase5-dev libpython3.5-dev
-                          #mingw-w64 mingw-w64-tools
+                          xdot pkg-config python3 \
+                          gcc-5-mingw-w64 gc++-5-mingw-w64 wine
   sudo apt-get autoremove -y
   sudo update-alternatives \
     --install /usr/bin/i686-w64-mingw32-gcc i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-5 60 \
@@ -68,9 +64,8 @@ fi
 if [ $ARCH == "windows_amd64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
-                          gcc-5-mingw-w64 gc++-5-mingw-w64 wine libeigen3-dev qtbase5-dev libpython3.5-dev
-                          #mingw-w64 mingw-w64-tools
+                          xdot pkg-config python3 \
+                          gcc-5-mingw-w64 gc++-5-mingw-w64 wine
   sudo apt-get autoremove -y
   sudo update-alternatives \
     --install /usr/bin/x86_64-w64-mingw32-gcc x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-5 60 \
@@ -88,8 +83,7 @@ if [ $ARCH == "darwin" ]; then
     brew update
   fi
   DEPS="bison flex gawk libffi git mercurial \
-        pkg-config python3 libusb gnu-sed wget \
-        boost boost-python3 eigen"
+        pkg-config python3 libusb gnu-sed wget"
   brew install --force $DEPS
   brew upgrade python
 fi
