32cadbaaea3e39b7750ed6d955c8b7c755fafadd
Sean Cross <sean@xobs.io>
2019-08-16 12:19:34 +0800

    create_package: zip with -r
    Need to explicitly do a recursive zip.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/create_package.sh
+++ scripts/create_package.sh
@@ -7,9 +7,9 @@ cd $PACKAGE_DIR/$NAME
 echo $VERSION > VERSION
 
 if [ $ARCH == "windows_x86" ]; then
-    zip ../$NAME-$ARCH-$VERSION.zip *
+    zip -r ../$NAME-$ARCH-$VERSION.zip *
 elif [ $ARCH == "windows_amd64" ]; then
-    zip ../$NAME-$ARCH-$VERSION.zip *
+    zip -r ../$NAME-$ARCH-$VERSION.zip *
 else
     tar -czvf ../$NAME-$ARCH-$VERSION.tar.gz *
 fi
