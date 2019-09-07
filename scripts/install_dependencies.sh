ed46664b3f53a7812161fb39d6b15e55461f9bb7
Sean Cross <sean@xobs.io>
2019-09-07 17:20:31 +0800

    dependencies: install rsync, make noninteractive
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 # Install dependencies script
 
 base_packages="build-essential bison flex libreadline-dev \
-               gawk tcl-dev libffi-dev git \
+               gawk tcl-dev libffi-dev git rsync \
                pkg-config python3"
 
 if [ $ARCH == "linux_x86_64" ]; then
@@ -13,7 +13,7 @@ fi
 
 if [ $ARCH == "linux_i686" ]; then
     sudo apt-get update
-    sudo apt-get install -y $base_packages \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $base_packages \
                             gcc-7-multilib g++-7-multilib
     gcc --version
     g++ --version
@@ -21,7 +21,7 @@ fi
 
 if [ $ARCH == "linux_armv7l" ]; then
     sudo apt-get update
-    sudo apt-get install -y $base_packages \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $base_packages \
                             gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf \
                             gcc-7-arm-linux-gnueabihf g++-7-arm-linux-gnueabihf \
                             binfmt-support qemu-user-static
@@ -31,7 +31,7 @@ fi
 
 if [ $ARCH == "linux_aarch64" ]; then
     sudo apt-get update
-    sudo apt-get install -y $base_packages \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $base_packages \
                             gcc-aarch64-linux-gnu g++-aarch64-linux-gnu \
                             binfmt-support qemu-user-static
     sudo apt-get autoremove -y
@@ -41,7 +41,7 @@ fi
 
 if [ $ARCH == "windows_x86" ]; then
     sudo apt-get update
-    sudo apt-get install -y $base_packages \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $base_packages \
                             mingw-w64 mingw-w64-tools mingw-w64-i686-dev \
                             zip
     i686-w64-mingw32-gcc --version
@@ -50,7 +50,7 @@ fi
 
 if [ $ARCH == "windows_amd64" ]; then
     sudo apt-get update
-    sudo apt-get install -y $base_packages \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $base_packages \
                             mingw-w64 mingw-w64-tools mingw-w64-x86-64-dev \
                             zip
     x86_64-w64-mingw32-gcc --version
