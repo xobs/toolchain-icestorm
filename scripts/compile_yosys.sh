81d1d273ff51387e63c5dd176f70b17460e75b9e
Sean Cross <sean@xobs.io>
2019-09-07 17:01:03 +0800

    scripts: mark as +x
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/compile_yosys.sh
+++ scripts/compile_yosys.sh
@@ -12,17 +12,17 @@ GIT_YOSYS=https://github.com/cliffordwolf/yosys.git
 cd $UPSTREAM_DIR
 
 if [ $REL -eq 1 ]; then
-  # -- Check and download the release
-  test -e $TAR_YOSYS || wget $REL_YOSYS
-  # -- Unpack the release
-  tar zxf $TAR_YOSYS
+    # -- Check and download the release
+    test -e $TAR_YOSYS || wget $REL_YOSYS
+    # -- Unpack the release
+    tar zxf $TAR_YOSYS
 else
-  # -- Clone the sources from github
-  git clone --depth=1 $GIT_YOSYS $YOSYS
-  git -C $YOSYS pull
-  echo ""
-  git -C $YOSYS reset --hard $VER
-  git -C $YOSYS log -1
+    # -- Clone the sources from github
+    git clone --depth=1 $GIT_YOSYS $YOSYS
+    git -C $YOSYS pull
+    echo ""
+    git -C $YOSYS reset --hard $VER
+    git -C $YOSYS log -1
 fi
 
 # -- Copy the upstream sources into the build directory
@@ -41,18 +41,18 @@ if [ $ARCH == "darwin" ]; then
                        ARCHFLAGS=\"$ABC_ARCHFLAGS\" ABC_USE_NO_READLINE=1"
 
 elif [ ${ARCH:0:7} == "windows" ]; then
-  make config-gcc
-  sed -i "s/-fPIC/-fpermissive/;" Makefile
-  sed -i "s/-Wall -Wextra -ggdb/-w/;" Makefile
-  sed -i "s/LD = gcc$/LD = $CC/;" Makefile
-  sed -i "s/CXX = gcc$/CXX = $CC/;" Makefile
-  sed -i "s/LDLIBS += -lrt/LDLIBS +=/;" Makefile
-  sed -i "s/LDFLAGS += -rdynamic/LDFLAGS +=/;" Makefile
-  make -j$J YOSYS_VER="$VER (Fomu build)" CPPFLAGS="-DYOSYS_WIN32_UNIX_DIR" \
-            LDLIBS="-static -lstdc++ -lm" \
-            ENABLE_TCL=0 ENABLE_PLUGINS=0 ENABLE_READLINE=0 ENABLE_COVER=0 ENABLE_ZLIB=0 \
-            ABCMKARGS="CC=\"$CC\" CXX=\"$CXX\" LIBS=\"-static -lm\" OPTFLAGS=\"-O\" \
-                       ARCHFLAGS=\"$ABC_ARCHFLAGS\" ABC_USE_NO_READLINE=1 ABC_USE_NO_PTHREADS=1 ABC_USE_LIBSTDCXX=1"
+    make config-gcc
+    sed -i "s/-fPIC/-fpermissive/;" Makefile
+    sed -i "s/-Wall -Wextra -ggdb/-w/;" Makefile
+    sed -i "s/LD = gcc$/LD = $CC/;" Makefile
+    sed -i "s/CXX = gcc$/CXX = $CC/;" Makefile
+    sed -i "s/LDLIBS += -lrt/LDLIBS +=/;" Makefile
+    sed -i "s/LDFLAGS += -rdynamic/LDFLAGS +=/;" Makefile
+    make -j$J YOSYS_VER="$VER (Fomu build)" CPPFLAGS="-DYOSYS_WIN32_UNIX_DIR" \
+              LDLIBS="-static -lstdc++ -lm" \
+              ENABLE_TCL=0 ENABLE_PLUGINS=0 ENABLE_READLINE=0 ENABLE_COVER=0 ENABLE_ZLIB=0 \
+              ABCMKARGS="CC=\"$CC\" CXX=\"$CXX\" LIBS=\"-static -lm\" OPTFLAGS=\"-O\" \
+                        ARCHFLAGS=\"$ABC_ARCHFLAGS\" ABC_USE_NO_READLINE=1 ABC_USE_NO_PTHREADS=1 ABC_USE_LIBSTDCXX=1"
 else
   make config-gcc
   sed -i "s/-Wall -Wextra -ggdb/-w/;" Makefile
